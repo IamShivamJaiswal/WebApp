06f8f355e688366385a871a167d69b0a58ae00d7
IamShivamJaiswal <shiva1210064@gmail.com>
2019-08-31 14:54:27 +0530

    modified

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
 
-    // Initialize the Image Classifier method with MobileNet
-    const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 
     // When the model is loaded
     function modelLoaded() {
-        console.log('Model Loaded!');
+        console.log("Model Loaded!");
     }
+
+    // Initialize the Image Classifier method with MobileNet
+    const classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+    
 
     var imgbase64;
     // Init
-    $('.image-section').hide();
-    $('.loader').hide();
-    $('#result').hide();
+    $(".image-section").hide();
+    $(".loader").hide();
+    $("#result").hide();
 
     // Upload Preview
     function readURL(input) {
@@ -20,34 +23,34 @@ $(document).ready(function () {
             var reader = new FileReader();
             reader.onload = function (e) {
                 imgbase64 = e.target.result;
-                $('#imagePreview').css('background-image', 'url(' + imgbase64 + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
+                $("#imagePreview").css("background-image", "url(" + imgbase64 + ")");
+                $("#imagePreview").hide();
+                $("#imagePreview").fadeIn(650);
             }
             reader.readAsDataURL(input.files[0]);
         }
     }
     $("#imageUpload").change(function () {
-        $('.image-section').show();
-        $('#btn-predict').show();
-        $('#result').text('');
-        $('#result').hide();
+        $(".image-section").show();
+        $("#btn-predict").show();
+        $("#result").text("");
+        $("#result").hide();
         readURL(this);
     });
 
     // Predict
-    $('#btn-predict').click(function () {
+    $("#btn-predict").click(function () {
         // Show loading animation
         $(this).hide();
-        $('.loader').show();
+        $(".loader").show();
         
         // Make a prediction with a selected image
         var image = new Image();
         image.src = imgbase64;
         classifier.predict(image, function(err, results) {
-            $('.loader').hide();
-            $('#result').fadeIn(600);
-            $('#result').text(' Result:  ' + results[0].label);
+            $(".loader").hide();
+            $("#result").fadeIn(600);
+            $("#result").text(" Result:  " + results[0].label);
         });
        
     });
